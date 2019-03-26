36e81b8affb072083ad97f48c9b9e508db0d7ae4
Mario O.M <marioortizmanero@gmail.com>
2019-03-26 01:25:02 +0100

    Redesign with mathematical formulas and better error handling

--- HS_Fatigue.py
+++ HS_Fatigue.py
@@ -1,64 +1,84 @@
+from collections import defaultdict
+
+
+# Default error message
 def error():
-    print("Whoops, something went wrong. Please try again.")
+    print("\nWhoops! Something went wrong. Please check if your input was valid.")
 
 
-def calc_mode(t, s):
-    d = 0
-    for i in range(s, t+s):
-        d = d + i
-    return d
-
-
-def lethal_mode(d_input, s):
-    d_calc = 0
-    i = 1
-    t = 0
-    while d_calc < d_input:
-        d_calc = d_calc + i
-        i += 1
-        t += 1
-    return t
-
-
-print("~~~~ HEARTHSTONE FATIGUE CALCULATOR by Mario O.M. ~~~~")
-
-while True:  # Checks if the mode input is valid
+# Handler for Ctrl+C forced exit
+def inputHandler(text):
     try:
-        mode = int(input("\nMODES: "
-                         "\n* '1' to get the turns needed for X damage "
-                         "\n* '2' to get the damage dealt in X turns "
-                         "\nPlease type to select your mode: "))
-    except ValueError:
-        error()
-        continue
+        return input(text)
+    except KeyboardInterrupt:
+        print("\nClosing the program...")
+        quit()
+
+
+# Gets the trapezoidal number starting at k
+# a.k.a. getting the total fatigue damage for calc mode
+# See README.md for more info about the formula
+def trapezoidalNumber(l,k):
+    return l*(2*(k-1)+l+1)/2 if (l>=0 and k>=0) else 0
+
+
+# Finds valid l in the previous formula (For lethal mode)
+# a.k.a. getting the number of turns to reach a fatigue damage
+def findTrapezoidalNumber(n,k):
+    if n>=0 and k>=0:
+        lCounter = 0
+        nCounter = trapezoidalNumber(lCounter,k)
+        while (nCounter < n):
+            lCounter += 1
+            nCounter = trapezoidalNumber(lCounter,k)
+        return lCounter
     else:
+        return 0
+
+
+# Mode 1: calculates turns needed for X cumulative fatigue damage
+def lethalMode():
+    while True:
+        # Cleanly getting the values:
         while True:
-            if mode == 1:  # Checks if the input after the mode is valid
-                while True:
-                    try:
-                        damageInput = int(input("\nPlease input the damage needed for lethal: "))
-                        startInput = int(input("Starting at how much damage? (default is 1): "))
-                    except ValueError:
-                        error()
-                        continue
-                    else:
-
-                        print("\n>> You will need %d fatigue turns for that sweet lethal"
-                              % lethal_mode(damageInput, startInput))
-                        continue
-
-            if mode == 2:
-                while True:
-                    try:
-                        turnsInput = int(input("\nPlease input the fatigue turns: "))
-                        startInput = int(input("Starting at how much damage? (default is 1): "))
-                    except ValueError:
-                        error()
-                        continue
-                    else:
-                        print("\n>> It will deal %d damage" % calc_mode(turnsInput, startInput))
-                        continue
-
-            else:
+            try:
+                maxDmg = int(inputHandler("\nPlease input the damage needed for lethal: "))
+                dmg = int(inputHandler("Starting at how much damage? (default is 1): ") or 1)
+            except ValueError:
                 error()
+            else:
                 break
+    
+        print("\n>> You will need %d fatigue turns for that sweet lethal" % findTrapezoidalNumber(maxDmg,dmg))
+
+
+# Mode 2: calculates cumulative fatigue damage
+def calcMode():
+    while True:
+        # Cleanly getting the values:
+        while True:
+            try:
+                turns = int(inputHandler("\nPlease input the fatigue turns: "))
+                dmg = int(inputHandler("Starting at how much damage? (default is 1): ") or 1)
+            except ValueError:
+                error()
+            else:
+                break
+
+        print("\n>> It will deal %d damage" % trapezoidalNumber(turns,dmg))
+
+
+def main():
+    # The input is obtained with a dictionary containing the mode functions
+    modesDict = defaultdict(lambda:error, { "1":lethalMode, "2":calcMode })
+
+    print("~ Hearthstone Fatigue Calculator by Mario O.M. ~")
+    while True:
+        mode = inputHandler("\n1. Lethal mode: turns needed for X damage"
+                            "\n2. Calculator mode: damage dealt in X turns"
+                            "\nPlease type the desired mode (1 or 2): ")
+        modesDict[mode]()
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,11 +1,38 @@
 # Hearthstone Fatigue Calculator
 Just a handy fatigue calculator, pretty useful when you're playing Mill Rogue or similar stuff and you're too lazy to calculate the lethal damage. 
 
-![Pic1](https://i.imgur.com/mIbLYWb.png)
-
-![Pic2](https://i.imgur.com/qiPJBQD.png)
+![](https://i.imgur.com/mIbLYWb.png)
 
 ## Modes
-1. You input the damage you need to get lethal, and you get the turns needed
-2. You input the fatigue turns, you get the damage you'll get from fatigue
-* You get to choose the initial fatigue damage in both modes
+1. **Lethal mode**: turns needed for X damage
+2. **Calculator mode**: damage dealt in X turns
+
+*Note: You get to choose the initial fatigue damage in both modes*
+
+## How it works
+The basics of this program are actually a very popular mathematical problem: the [triangular numbers](https://en.wikipedia.org/wiki/Triangular_number). They count objects arranged in an equilateral triangle, as in the diagram on the bottom:
+
+![](https://upload.wikimedia.org/wikipedia/commons/1/1c/First_six_triangular_numbers.svg)
+
+This is basically the sum of the dots of each row in our triangle:
+
+![](https://wikimedia.org/api/rest_v1/media/math/render/svg/8411fe69f79f2971b7c7a453b0b547bb37e9f6df)
+
+But for fatigue damage, you don't always start at 1 dot. Maybe you're already 3 turns in and you want to calculate the damage left. That's when trapezoidal numbers come into play. You can learn more about them [on this paper by Carlton Gamer, David W. Roeder and John J. Watkins](https://www.jstor.org/stable/2689901?origin=crossref&seq=1#metadata_info_tab_contents).
+
+![](https://i.imgur.com/GsaR1wo.png)
+
+ A trapezoidal triangle can be expressed as the entire triangular number (until `k+l`) minus the small triangular number (until `k`). The formula then can be simplified to:
+ 
+ ![](https://i.imgur.com/vajUmq4.png)
+ 
+ to obtain the trapezoidal number beginning with `k+1`. Now, to find the turns given the total fatigue damage, we could use the solved formula for `l`:
+
+ ![](https://i.imgur.com/wTHGwW9.png)
+
+ But as it's a quadratic equation and not as exact and simple as I'd want it to be, the program is just designed to loop until it finds the requested trapezoidal result. If you think of a better method, don't doubt in opening an issue in this repository and letting me know!
+ 
+Other sources:
+
+* [C. Gamer, D. W. Roeder, and J. J. Watkins, “Trapezoidal Numbers,” Mathematics Magazine, vol. 58, no. 2, pp. 108–110, 1985](https://www.hindawi.com/journals/ijmms/2017/4515249/)
+* [R. Guy, “Sums of consecutive integers,” The Fibonacci Quarterly. Official Organ of the Fibonacci Association, vol. 20, no. 1, pp. 36–38, 1982](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.388.5889&rep=rep1&type=pdf)
